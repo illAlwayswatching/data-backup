d326a04afced6679b75c619abb8ec747c7e7fbc6
abel-chai <1401325755@qq.com>
2024-01-05 22:31:09 +0800

    后端架构搭建

---
+++ data-backup-back/src/main/java/org/example/databackupback/DataBackupBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.databackupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBackupBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBackupBackApplication.class, args);
+    }
+
+}
---
+++ data-backup-back/src/test/java/org/example/databackupback/DataBackupBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.databackupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBackupBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
